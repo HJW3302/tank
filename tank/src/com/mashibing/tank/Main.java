3522c4840692ec1e5d5c97b2a6b5967073149078
Administrator <1135015668@qq.com>
2021-02-16 11:03:22 +0800

    每隔50ms刷新窗口

---
+++ tank/src/com/mashibing/tank/Main.java
@@ -0,0 +1,11 @@
+package com.mashibing.tank;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		TankFrame tankFrame = new TankFrame();
+		while (true) {
+			Thread.sleep(100);
+			tankFrame.repaint();
+		}
+	}
+}
--- tank/src/com/mashibing/tank/TankFrame.java
+++ tank/src/com/mashibing/tank/TankFrame.java
@@ -1,18 +1,28 @@
 package com.mashibing.tank;
 
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.HeadlessException;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class T {
-	public static void main(String[] args) {
-		Frame f = new Frame();
-		f.setSize(800,600);
-		f.setResizable(false);
-		f.setTitle("坦克-tank war-大战");
+public class TankFrame extends Frame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	int x =200, y=200;
+
+	public TankFrame() throws HeadlessException {
+		setSize(800,600);
+		setResizable(false);
+		setTitle("坦克-tank war-大战");
 		
-		f.setVisible(true);
-		f.addWindowListener(new WindowListener() {
+		setVisible(true);
+		this.addKeyListener(new MyKeyLister());
+		addWindowListener(new WindowListener() {
 			
 			@Override
 			public void windowOpened(WindowEvent e) {
@@ -43,18 +53,43 @@ public class T {
 				// TODO Auto-generated method stub
 				System.exit(0);
 			}
-			
+
 			@Override
 			public void windowClosed(WindowEvent e) {
 				// TODO Auto-generated method stub
 				
 			}
-			
+
 			@Override
 			public void windowActivated(WindowEvent e) {
 				// TODO Auto-generated method stub
 				
 			}
+
 		});
 	}
+
+	@Override
+	public void paint(Graphics g) {
+		System.out.println("paint");
+		g.fillRect(x, y, 50, 50);
+		x += 10;
+	//	y += 10;
+		//super.paint(g);
+	}
+	class MyKeyLister extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			//x+= 200;
+			//repaint();
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("keyReleased");
+		}
+		
+	}
+	
+	
 }
